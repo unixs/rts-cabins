fa3f967e86395c5f43b27d477f503e1a7b7c5563
Alexander Feodorov <vester@unixcomp.org>
2023-10-19 16:38:46 +0400

    inproject parser

--- cmake/FindStfParser.cmake
+++ cmake/FindStfParser.cmake
@@ -5,13 +5,34 @@ find_package(Java 17.0 REQUIRED)
 include(CMakePrintHelpers)
 include(UseJava)
 
-find_jar(StfParser_JAR stf-parser
+set(JAR_VERSION 0.1.2)
+set(JAR_FILENAME parser-${JAR_VERSION})
+set(JAR_URL https://github.com/unixs/stf-parser/releases/download/v${JAR_VERSION}/parser-${JAR_VERSION}-jar-with-dependencies.jar)
+set(JAR_MD5 307ff81517568d86e051490fd7363d36)
+set(JAR_PROJECT_PATH ${PROJECT_SOURCE_DIR}/tools/${JAR_FILENAME}.jar)
+
+message(STATUS "Checking parser jar.")
+
+if(NOT EXISTS ${JAR_PROJECT_PATH})
+	message(STATUS "Downloading jar...")
+	file(DOWNLOAD ${JAR_URL}
+		${JAR_PROJECT_PATH}
+		STATUS JAR_DOWNLOAD_STATUS
+		SHOW_PROGRESS)
+
+	list(GET JAR_DOWNLOAD_STATUS 0 JAR_DOWNLOAD_RESULT_CODE)
+
+	if(NOT JAR_DOWNLOAD_RESULT_CODE EQUAL 0)
+		message(FATAL_ERROR "Failed downloading! Error: ${JAR_DOWNLOAD_STATUS}.")
+	endif()
+endif()
+
+find_jar(StfParser_JAR ${JAR_FILENAME}
 	NAMES
-	rts-stf-parser-0.1.1-jar-with-dependencies
-	rts-stf-parser
-	rts-stf-parser-jar-with-dependencies
-	rts-parser
-	rts-stf-parser-with-dependencies
+	parser-${JAR_VERSION}-jar-with-dependencies
+	stf-parser
+	stf-parser-jar-with-dependencies
+	parser
 	stf-parser-with-dependencies
 	REQUIRED
 	NO_CMAKE_FIND_ROOT_PATH
@@ -29,18 +50,28 @@ find_package_handle_standard_args(StfParser
 	StfParser_JAR
 )
 
-cmake_print_variables(StfParser_JAR)
-
+# cmake_print_variables(StfParser_JAR)
 function(stf_parser_command source)
-	set(HEADER_EXTENSION .hpp)
+	set(HEADER_EXTENSION hpp)
 
 	get_filename_component(OUTPUT_DIR ${source} DIRECTORY CACHE)
 	get_filename_component(OUTPUT_FILE ${source} NAME_WE CACHE)
 
 	add_custom_command(
-		OUTPUT "${OUTPUT_DIR}/${OUTPUT_FILE}.${HEADER_EXTENSION}"
+		OUTPUT ${OUTPUT_DIR}/${OUTPUT_FILE}.${HEADER_EXTENSION}
 		COMMAND ${Java_JAVA_EXECUTABLE} -jar ${StfParser_JAR} ${CMAKE_CURRENT_LIST_DIR}/${source}
-		DEPENDS ${CMAKE_CURRENT_LIST_DIR}/${source}
+		MAIN_DEPENDENCY ${CMAKE_CURRENT_LIST_DIR}/${source}
+		COMMENT "Compiling model files..."
 		VERBATIM
 	)
 endfunction()
+
+mark_as_advanced(
+	JAR_FILENAME
+	JAR_VERSION
+	JAR_URL
+	JAR_MD5
+	JAR_DOWNLOAD_STATUS
+	JAR_DOWNLOAD_RESULT_CODE
+	JAR_PROJECT_PATH
+)
